72811002d5864a4e870cb1328e197ebff5d3c40c
Aleebaba001 <aliyumuhammed523@gmail.com>
2023-09-22 09:40:53 +0100

    new changes

--- puppeteer_refresherX.js
+++ puppeteer_refresherX.js
@@ -8,11 +8,13 @@ const puppeteer = require("puppeteer");
         headless: false
        });
 
+       await page.setDefaultNavigationTimeout(100000);
        const page = await browser.newPage();
 
        //Go to the new page
 
-       await page.goto('https://uat.zoftwarehub.com')
+       await page.setDefaultNavigationTimeout(100000);
+       await page.goto('https://uat.zoftwarehub.com');
 
        console.log("Open the Zoftware UAT website")
 
@@ -20,15 +22,19 @@ const puppeteer = require("puppeteer");
 
        //Enter a question into the chatbot
 
-       const botSearchField = document.querySelector('textarea[class="bot-input ng-untouched ng-pristine ng-valid"]');
+       const botSearchField = 'textarea[class="bot-input ng-untouched ng-pristine ng-valid"]';
 
+       await page.setDefaultNavigationTimeout(10000);
        await page.waitForBotSearchField(botSearchField)
 
+       await page.setDefaultNavigationTimeout(10000);
        await page.click(botSearchField, {clickCount: 1})
        console.log("click on the bot search field")
 
+       await page.setDefaultNavigationTimeout(10000);
        await page.type(botSearchField, "Recommend an app that will help me lose weight in 30 seconds");
        console.log("enter a question into the bot search field")
+
 
     } 
 )()
